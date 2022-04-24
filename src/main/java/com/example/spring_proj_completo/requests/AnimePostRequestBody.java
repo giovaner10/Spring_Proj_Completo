594c2399c696238bf14155e574407d62a807aa1b
giovaner10 <giovanerr10@gmail.com>
2022-04-24 14:21:13 -0300

    objeto dto usado para a postagem de um novo anime

--- src/main/java/com/example/spring_proj_completo/requests/AnimePostRequestBody.java
+++ src/main/java/com/example/spring_proj_completo/requests/AnimePostRequestBody.java
@@ -4,8 +4,13 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class AnimePostRequestBody {
+    @NotNull
     private String name;
 }

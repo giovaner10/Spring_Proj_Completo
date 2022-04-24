78f50e5fef47b6c9fe5f532f7c29b1185cc7d835
giovaner10 <giovanerr10@gmail.com>
2022-04-24 14:23:41 -0300

    AnimeService

--- src/main/java/com/example/spring_proj_completo/service/AnimeService.java
+++ src/main/java/com/example/spring_proj_completo/service/AnimeService.java
@@ -2,12 +2,16 @@ package com.example.spring_proj_completo.service;
 
 import com.example.spring_proj_completo.config.AnimeAssembler;
 import com.example.spring_proj_completo.domain.Anime;
+import com.example.spring_proj_completo.exception.BadRequestException;
 import com.example.spring_proj_completo.repository.AnimeRepository;
 import com.example.spring_proj_completo.requests.AnimePostRequestBody;
 import com.example.spring_proj_completo.requests.AnimePutRequestBody;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -19,8 +23,8 @@ public class AnimeService {
     private final AnimeRepository animeRepository;
     private final AnimeAssembler animeAssembler;
 
-    public List<Anime> listAll(){
-        return animeRepository.findAll();
+    public Page<Anime> listAll(Pageable pageable){
+        return animeRepository.findAll(pageable);
     }
 
     public List<Anime> findByName(String name){
@@ -30,9 +34,10 @@ public class AnimeService {
     public Anime findByIdOrElseBadRequestException(Long id){
         return animeRepository.findById(id)
                 .orElseThrow(()->
-                        new ResponseStatusException(HttpStatus.BAD_REQUEST, "NOT FOUND"));
+                        new BadRequestException("NOT FOUND"));
     }
 
+    @Transactional
     public Anime save(AnimePostRequestBody animePostRequestBody){
         Anime newAnime = animeAssembler.toAnimeSave(animePostRequestBody);
         return animeRepository.save(newAnime);

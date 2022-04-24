0d38cc1d10724ba15da9deff149b0a9d1f37630b
giovaner10 <giovanerr10@gmail.com>
2022-04-24 14:24:01 -0300

    AnimeController

--- src/main/java/com/example/spring_proj_completo/controller/AnimeController.java
+++ src/main/java/com/example/spring_proj_completo/controller/AnimeController.java
@@ -5,10 +5,13 @@ import com.example.spring_proj_completo.requests.AnimePostRequestBody;
 import com.example.spring_proj_completo.requests.AnimePutRequestBody;
 import com.example.spring_proj_completo.service.AnimeService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,8 +22,8 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public ResponseEntity<List<Anime>> list(){
-        return ResponseEntity.ok(animeService.listAll());
+    public ResponseEntity<Page<Anime>> list(Pageable pageable){
+        return ResponseEntity.ok(animeService.listAll(pageable));
     }
 
     @GetMapping(path = "/findname")
@@ -34,7 +37,7 @@ public class AnimeController {
     }
 
     @PostMapping
-    public ResponseEntity<Anime> save(@RequestBody AnimePostRequestBody anime){
+    public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostRequestBody anime){
         return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
     }
 

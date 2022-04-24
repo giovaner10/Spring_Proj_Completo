237ef1dfab9c4c4b7d5ec60d977d929071bc1c8b
giovaner10 <giovanerr10@gmail.com>
2022-04-23 23:42:33 -0300

    modelo para a transferencia de objetos

---
+++ src/main/java/com/example/spring_proj_completo/requests/AnimePostRequestBody.java
@@ -0,0 +1,11 @@
+package com.example.spring_proj_completo.requests;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnimePostRequestBody {
+    private String name;
+}
---
+++ src/main/java/com/example/spring_proj_completo/requests/AnimePutRequestBody.java
@@ -0,0 +1,12 @@
+package com.example.spring_proj_completo.requests;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnimePutRequestBody {
+
+    private Long id;
+    private String name;
+}

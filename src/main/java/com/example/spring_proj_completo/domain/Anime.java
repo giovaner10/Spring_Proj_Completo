2ad3c9b0e3d76c3cde81d713a06bec07a14bfb09
giovaner10 <giovanerr10@gmail.com>
2022-04-23 23:39:00 -0300

    model principal

--- src/main/java/com/example/spring_proj_completo/domain/Anime.java
+++ src/main/java/com/example/spring_proj_completo/domain/Anime.java
@@ -1,13 +1,22 @@
 package com.example.spring_proj_completo.domain;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Anime {
 
-    private String nome;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
 }

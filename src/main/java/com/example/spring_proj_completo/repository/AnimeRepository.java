4479dd5bf6ddfc22ec6fba65f1e9681b612be87f
giovaner10 <giovanerr10@gmail.com>
2022-04-23 23:42:05 -0300

    interface repository junto ao jpa

---
+++ src/main/java/com/example/spring_proj_completo/repository/AnimeRepository.java
@@ -0,0 +1,13 @@
+package com.example.spring_proj_completo.repository;
+
+import com.example.spring_proj_completo.domain.Anime;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
+
+    List<Anime> findByName(String name);
+}

d2d0553ef3b250b0fc2327811bab00662940d347
giovaner10 <giovanerr10@gmail.com>
2022-04-28 13:34:26 -0300

    adicao de validacoes do tipo javax constraints

--- src/main/java/com/example/spring_proj_completo/domain/Anime.java
+++ src/main/java/com/example/spring_proj_completo/domain/Anime.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
@@ -18,5 +19,7 @@ public class Anime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
     private String name;
 }

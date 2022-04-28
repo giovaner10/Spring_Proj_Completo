7c5fa0cf1407a6604e67d2184f599bc711a02586
giovaner10 <giovanerr10@gmail.com>
2022-04-28 13:31:53 -0300

    anime put request body

--- src/main/java/com/example/spring_proj_completo/requests/AnimePutRequestBody.java
+++ src/main/java/com/example/spring_proj_completo/requests/AnimePutRequestBody.java
@@ -1,10 +1,12 @@
 package com.example.spring_proj_completo.requests;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class AnimePutRequestBody {
 
     private Long id;
